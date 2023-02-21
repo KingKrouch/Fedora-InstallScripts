ec9d8e51647cd1a1ab9d7eef0042ccbe430c18f0
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2023-02-20 22:55:21 -0500

    Fix typo with plugins

--- Fedora_Setup.sh
+++ Fedora_Setup.sh
@@ -60,7 +60,7 @@ wget -O ~/.p10k.zsh https://github.com/KingKrouch/Fedora-InstallScripts/raw/main
 # Set up some ZSH plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i 's/plugins=(git)/plugins=(git, zsh-syntax-highlighting, zsh-autosuggestions)/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/g' ~/.zshrc
 
 ## Add nerd-fonts for Noto and SourceCodePro font families. This will just install everything together, but I give no fucks at this point, just want things a little easier to set up.
 git clone https://github.com/ryanoasis/nerd-fonts.git && cd nerd-fonts && ./install.sh && cd .. && sudo rm -rf nerd-fonts
