f9d01f6810115daef4742f9ad80de75f0454bcff
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2023-05-31 21:02:35 -0400

    Create fix.sh

---
+++ fixes/mudbox/fix.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo cp -rf Autodesk-Mudbox.desktop /usr/autodesk/mudbox2024/desktop/
+sudo cp -rf Autodesk-Mudbox.directory /usr/autodesk/mudbox2024/desktop/
+sudo cp -rf Mudbox.png /usr/autodesk/mudbox2024/desktop/
