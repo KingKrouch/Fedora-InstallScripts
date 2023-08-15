ddf454807182bcefdb2ac01539a4c59e72c85c7e
KingKrouch <64479044+KingKrouch@users.noreply.github.com>
2023-08-15 12:47:32 -0400

    Update Fedora_Setup.sh

--- Fedora_Setup.sh
+++ Fedora_Setup.sh
@@ -320,8 +320,7 @@ flatpak install flathub io.github.achetagames.epic_asset_manager $FLATPAK_TYPE -
 # Install Docker.
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin gnome-terminal -y
-sudo dnf install https://desktop.docker.com/linux/main/amd64/docker-desktop-4.20.1-x86_64.rpm -y
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo groupadd docker && sudo usermod -aG docker $USER
 sudo systemctl enable docker && sudo systemctl start docker
 
